18adfe1d890aae798b212242f69fd3e59c61c55c
Valdir Torres <valdyrtorres@yahoo.com.br>
2018-10-18 01:10:13 -0300

    Retrofit framework adicionado

--- ListaDeFrutas/app/src/main/java/br/com/valdir/desafiolistafrutas/MainActivity.java
+++ ListaDeFrutas/app/src/main/java/br/com/valdir/desafiolistafrutas/MainActivity.java
@@ -1,13 +1,31 @@
 package br.com.valdir.desafiolistafrutas;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+
+import br.com.valdir.desafiolistafrutas.api.APIServiceGet;
+import br.com.valdir.desafiolistafrutas.data.FrutasContract;
 import br.com.valdir.desafiolistafrutas.sync.FrutasSyncAdapter;
+import retrofit2.Call;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.Callback {
 
@@ -37,7 +55,69 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Call
         MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.main_fragment);
         mainFragment.setUseFrutaDestaque(false);
 
-        FrutasSyncAdapter.initializeSyncAdapter(this);
+        // Esse método não utiliza o retrofit
+        //FrutasSyncAdapter.initializeSyncAdapter(this);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://raw.githubusercontent.com/").build();
+
+        APIServiceGet api = retrofit.create(APIServiceGet.class);
+
+        api.getListaFrutas().enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    JSONObject jObj = new JSONObject(response.body().string());
+
+                    JSONArray jArr = jObj.getJSONArray("fruits");
+                    for (int i=0; i < jArr.length(); i++) {
+                        JSONObject obj = jArr.getJSONObject(i);
+                        Log.d("DesafioListaFrutas", obj.getString("name"));
+                        Log.d("DesafioListaFrutas", obj.getString("image"));
+                        Log.d("DesafioListaFrutas", obj.getString("price"));
+                    }
+
+                    List<ItemFruta> itemFrutas = JsonUtil.fromJsonArrayToList(jArr);
+
+                    if (itemFrutas == null) {
+                        return;
+                    }
+
+                    for (ItemFruta itemFruta : itemFrutas) {
+                        ContentValues values = new ContentValues();
+                        values.put(FrutasContract.FrutaEntry._ID, itemFruta.getId());
+                        values.put(FrutasContract.FrutaEntry.COLUMN_NAME, itemFruta.getName());
+                        values.put(FrutasContract.FrutaEntry.COLUMN_PRICE, itemFruta.getPrice());
+                        values.put(FrutasContract.FrutaEntry.COLUMN_IMAGE_PATH, itemFruta.getImage());
+
+                        // Preparar para o futuro caso a lista de produtos aumente no JSON fornecido
+                        int update = getContentResolver().update(FrutasContract.FrutaEntry.buildUriForFrutas(itemFruta.getId()),
+                                values, null, null);
+
+                        if (update == 0) {
+                            getContentResolver().insert(FrutasContract.FrutaEntry.CONTENT_URI, values);
+                        }
+                    }
+
+//                    Frutas frutas = new Frutas();
+//
+//                    frutas.setFrutaList();
+
+                    Log.d("RetrofitTutorial", response.body().string());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+
 
     }
 
--- README.md
+++ README.md
@@ -1,19 +1,40 @@
-1 - O aplicativo inicia mostrando uma lista, um RecycledView object preenchido com a imagem da fruta, o nome da fruta e o preço em dolar
-2 - Ao clicar no item em questão, é mostrado o detalhe com a imagem da fruta em plano maior, seu nome em destaque e o preço em dolar e o
+1 - O aplicativo inicia mostrando uma lista, um RecycledView object 
+    preenchido com a imagem da fruta, o nome da fruta e o preço em dolar
+2 - Ao clicar no item em questão, é mostrado o detalhe com a imagem da 
+    fruta em plano maior, seu nome em destaque e o preço em dolar e o
     preço em real convertido por uma chamada nativa em C.
     
 Decisões tomadas:
-1 - Para fazer o cache de imagens optou-se por armazená-las em um banco SQLite, ver detalhes na classe FrutasDBHelper
-2 - Optou-se utilizar a arquitetura (MVP - Model, View , Presenter) em que para fazer a inteface com o model foi criado um
-contrato (ver classe FrutasContract)
+1 - Para fazer o cache de imagens optou-se por persistí-las no 
+    SQLite, ver detalhes na classe FrutasDBHelper
+2 - Optou-se utilizar a arquitetura (MVP - Model, View , Presenter) 
+    em que para fazer a inteface com o model foi criado um
+    contrato (ver classe FrutasContract)
+    
+View - em nosso caso é o MainFragment.java e FrutaDetalheFragment que mostram 
+respectivamente a lista e o detalhe dos dados, não contém regra alguma do negócio 
+a não ser disparar eventos que notificam mudança de estado dos dados que ele exibe e 
+processamento próprio dele. O RecycledView utilizado implementa uma interface que 
+expõe campos e eventos que o presenter necessita.
 
-View - em nosso caso é o MainFragment.java e FrutaDetalheFragment que mostram respectivamente a lista e o detalhe dos dados, 
-não contém regra alguma do negócio a não ser disparar eventos que notificam mudança de estado dos dados que ele exibe e 
-processamento próprio dele. O RecycledView utilizado implementa uma interface que expõe campos e eventos que o presenter necessita.
+Model - São os objetos que serão manipulados. Um objeto Model implementa uma interface 
+que expõe os campos que o presenter irá atualizar quando sofrerem alteração na view. 
+Ver classe ItemFruta.java
 
-Model - São os objetos que serão manipulados. Um objeto Model implementa uma interface que expõe os campos que o presenter 
-irá atualizar quando sofrerem alteração na view. Ver classe ItemFruta.java
+Presenter - É a ligação entre View e Model, possui papel de mediador entre eles. Ele é 
+encarregado de atualizar o view quando o model é alterado e de sincronizar o model em 
+relação ao view. Em nosso caso é o MainActivity.java que congrega o acesso por meio da 
+interface APIServiceGET.java que faz o elo com o webservice e CalculeNative.java que é 
+o elo com o programa em C (cambio-lib.cpp) que faz a conversão de dolar para real.
 
-Presenter - É a ligação entre View e Model, possui papel de mediador entre eles. Ele é encarregado de atualizar o view
-quando o model é alterado e de sincronizar o model em relação ao view. Em nosso caso é o FrutasSyncAdapter.java e o 
-CalculeNative.java que chama o programa em C (cambio-lib.cpp) que faz a conversão de dolar para real.
+3 - Foi utilizado o Retrofit como framework de comunicação com a API
+4 - Optou-se por não ser utilizado o butterknife, pois o uso do findViewById 
+    nas versões mais recentes do Android não exige mais o uso expliícito do cast, 
+    além disso o uso é plenamente dispensável caso seja utilizado o Kotlin e 
+    facilita sua migração para esta linguagem.
+5 - Optou-se utilizar o CMakeList.txt em vez do Android.mk, pois o App foi construído 
+    no Android Studio versão 3.2.1 em que é requerida uma menor complexidade na montagem 
+    do ambiente e sobretudo pela indisponibilidade de um ambiente confiável a ser
+    montado no Windows 10 para uso do GNU Tools. Se tivesse um ambiente Linux poderia 
+    ser feito com mais praticidade o uso do Android.mk e ndk-build em vez do CMake.
+
